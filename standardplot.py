00da2b9e02261e1194ac4627e6525a05533bf3be
Jizhou Wang <jizhouwang@gmail.com>
2018-01-30 03:37:53 -0500

    hillclimbing need test

--- standardplot.py
+++ standardplot.py
@@ -4,31 +4,72 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+from decimal import Decimal
 
 #xplot = np.linspace(0, 30, 10000)
 
-y = 0
-x = 0
-dx = 0
+
 plt.xlabel('x axis label')
 plt.ylabel('y axis label')
 plt.title('hill climbing')
-# plt.legend(['t', 't**2', 't**3'])
+plt.legend(['t', 't**2', 't**3'])
 
-while dx < 0.1:
-    #BUG TODO rounding issue need fix
-    dx += 0.01
-    while x <= 10:
-        
-        square = (x)**2
-        sint = np.sin(square/2)
-        log2 = np.log2(x+4)
-        y = sint/log2
 
-        plt.plot(x, y, 'rs')
-        x = x + dx
-    plt.show()
-    x=0
+dx = 0
+
+def function(x):
+    square = (x)**2
+    sint = np.sin(square/2)
+    logg2 = np.log2(x+4)
+    y = sint/logg2
+    return y
+
+#increase the step size
+
+def Hillclimbing():
+    #increase the step size
+    while dx < round(0.1,1):
+        dx += round(0.01,2)
+        xstart=x = 0
+        #increase the starting position
+        while xstart <= 10:
+            #reinitialize
+
+            step = 0
+
+            yOP=yPOP= function(x)
+            plt.plot(x, yOP, 'r.')
+
+            #loop while yOP is increasing and x is less than 10
+            while (yOP>yPOP and x <= 10) or step == 0:
+
+                neighbours = set()
+                if round(x-dx, 2)>=0:
+                    neighbours.add(round(x-dx,2))
+                if round(x+dx, 2)>= 0:
+                    neighbours.add(round(x+dx,2))
+                step += 1
+                print(step)
+                xtemp=x
+                yPOP = yOP
+                #explore all the neighbours
+                for xn in list(neighbours):
+                    y = function(xn+xtemp)
+                    neighbours.remove(xn)
+                    if yOP < y:
+                        yOP = y
+                        x = xn + xtemp
+                        plt.plot(xn+xtemp, y, 'r.')
+                    #TODO plateau?
+                    elif yOP == y:
+                        pass
+            plt.show()
+            xstart+=1
+            x=xstart
+
+Hillclimbing()
+
+    
     
 
 
